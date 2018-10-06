c146c7a1745a5349ed0257060d94c1863199e49d
deshtros <deshtros@gmail.com>
2018-10-07 01:13:32 +0200

    refacto

--- H-PF/HesoControl/NumberPicker/NumberPicker.xaml.cs
+++ H-PF/HesoControl/NumberPicker/NumberPicker.xaml.cs
@@ -20,83 +20,59 @@ namespace H_PF.HesoControl.NumberPicker
     /// </summary>
     public partial class NumberPicker : UserControl
     {
-        private NumberPicker_DataContext _context;
 
         // DependencyProperties
+        public static readonly DependencyProperty MinValueProperty = DependencyProperty.Register("MinValue", typeof(int), typeof(NumberPicker), new PropertyMetadata(0));
+        public static readonly DependencyProperty MaxValueProperty = DependencyProperty.Register("MaxValue", typeof(int), typeof(NumberPicker), new PropertyMetadata(2147483647));
+        public static readonly DependencyProperty CenterBackgroundProperty = DependencyProperty.Register("CenterBackground", typeof(Brush), typeof(NumberPicker), new PropertyMetadata(Brushes.White));
+        public static readonly DependencyProperty CenterForegroundProperty = DependencyProperty.Register("CenterForeground", typeof(Brush), typeof(NumberPicker), new PropertyMetadata(Brushes.Black));
+        public static readonly DependencyProperty CenterBorderBrushProperty = DependencyProperty.Register("CenterBorderBrush", typeof(Brush), typeof(NumberPicker), new PropertyMetadata(Brushes.LightGray));
+        public static readonly DependencyProperty ButtonBackgroundProperty = DependencyProperty.Register("ButtonBackground", typeof(Brush), typeof(NumberPicker), new PropertyMetadata(Brushes.LightGray));
+        public static readonly DependencyProperty ButtonForegroundProperty = DependencyProperty.Register("ButtonForeground", typeof(Brush), typeof(NumberPicker), new PropertyMetadata(Brushes.Black));
+        public static readonly DependencyProperty ButtonBorderBrushProperty = DependencyProperty.Register("ButtonBorderBrush", typeof(Brush), typeof(NumberPicker), new PropertyMetadata(Brushes.Gray));
 
         public int MinValue
         {
             get { return (int)GetValue(MinValueProperty); }
             set { SetValue(MinValueProperty, value); }
         }
-
-        public static readonly DependencyProperty MinValueProperty =
-            DependencyProperty.Register("MinValue", typeof(int), typeof(NumberPicker), new PropertyMetadata(0));
-
         public int MaxValue
         {
             get { return (int)GetValue(MaxValueProperty); }
             set { SetValue(MaxValueProperty, value); }
         }
-
-        public static readonly DependencyProperty MaxValueProperty =
-            DependencyProperty.Register("MaxValue", typeof(int), typeof(NumberPicker), new PropertyMetadata(2147483647));
-
         public Brush CenterBackground
         {
             get { return (Brush)GetValue(CenterBackgroundProperty); }
             set { SetValue(CenterBackgroundProperty, value); }
         }
-
-        public static readonly DependencyProperty CenterBackgroundProperty =
-            DependencyProperty.Register("CenterBackground", typeof(Brush), typeof(NumberPicker), new PropertyMetadata(Brushes.White));
-
-        public Brush ButtonBackground
-        {
-            get { return (Brush)GetValue(ButtonBackgroundProperty); }
-            set { SetValue(ButtonBackgroundProperty, value); }
-        }
-
-        public static readonly DependencyProperty ButtonBackgroundProperty =
-            DependencyProperty.Register("ButtonBackground", typeof(Brush), typeof(NumberPicker), new PropertyMetadata(Brushes.LightGray));
-
         public Brush CenterForeground
         {
             get { return (Brush)GetValue(CenterForegroundProperty); }
             set { SetValue(CenterForegroundProperty, value); }
         }
-
-        public static readonly DependencyProperty CenterForegroundProperty =
-            DependencyProperty.Register("CenterForeground", typeof(Brush), typeof(NumberPicker), new PropertyMetadata(Brushes.Black));
-
-        public Brush ButtonForeground
-        {
-            get { return (Brush)GetValue(ButtonForegroundProperty); }
-            set { SetValue(ButtonForegroundProperty, value); }
-        }
-
-        public static readonly DependencyProperty ButtonForegroundProperty =
-            DependencyProperty.Register("ButtonForeground", typeof(Brush), typeof(NumberPicker), new PropertyMetadata(Brushes.Black));
-
         public Brush CenterBorderBrush
         {
             get { return (Brush)GetValue(CenterBorderBrushProperty); }
             set { SetValue(CenterBorderBrushProperty, value); }
         }
-
-        public static readonly DependencyProperty CenterBorderBrushProperty =
-            DependencyProperty.Register("CenterBorderBrush", typeof(Brush), typeof(NumberPicker), new PropertyMetadata(Brushes.LightGray));
-
+        public Brush ButtonBackground
+        {
+            get { return (Brush)GetValue(ButtonBackgroundProperty); }
+            set { SetValue(ButtonBackgroundProperty, value); }
+        }
+        public Brush ButtonForeground
+        {
+            get { return (Brush)GetValue(ButtonForegroundProperty); }
+            set { SetValue(ButtonForegroundProperty, value); }
+        }
         public Brush ButtonBorderBrush
         {
             get { return (Brush)GetValue(ButtonBorderBrushProperty); }
             set { SetValue(ButtonBorderBrushProperty, value); }
         }
 
-        public static readonly DependencyProperty ButtonBorderBrushProperty =
-            DependencyProperty.Register("ButtonBorderBrush", typeof(Brush), typeof(NumberPicker), new PropertyMetadata(Brushes.Gray));
-
-        // DependencyProperties
+        private NumberPicker_DataContext _context;
 
         public NumberPicker()
         {
